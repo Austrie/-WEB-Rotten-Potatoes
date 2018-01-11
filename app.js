968f148a26cd270110a524a6afd671aed0398886
Shane Austrie <austrie@Shanes-MacBook-Pro.local>
2018-01-11 13:11:11 -0800

    Initial stages of gettig MongoDB working

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const app = express();
 
+//We're using MongoDB, so we're using Mongoose for mapping JSON to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+const Review = mongoose.model('Review', {
+  title: String
+});
+
+
 //We're also using Handlebars for ExpressJS
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -14,21 +22,27 @@ app.set('view engine', 'handlebars');
 //    res.render('home', {msg: 'Hello World!'});
 //});
 //
-//app.listen(3000, () => {
-//    console.log('App listening on port 3000!');
-//});
 
 //Our mock array of projects
-let reviews = [
-    { title: "Great Review" },
-    { title: "Next Review" }
-];
+// let reviews = [
+//     { title: "Great Review" },
+//     { title: "Next Review" }
+// ];
 
-//Index
 app.get('/reviews', (req, res) => {
     res.render('reviews-index', { reviews });
 });
 
+//Index
 app.get('/', (req, res) => {
-    res.render('reviews-index', {reviews});
+  Review.find().then((reviews) => {
+    res.render('reviews-index', {reviews: reviews});
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+//Listens for port 3000: E.g. localhost:3000s
+app.listen(3000, () => {
+   console.log('App listening on port 3000!');
 });
