8f4665caf14f29bf3d73cfd039d5e5d3d980335d
Shane Austrie <austrie@Shanes-MacBook-Pro.local>
2018-01-11 22:58:02 -0800

    Added the functionality to list reviews, see specific reviews, add reviews, and delete reviews

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 const express = require('express');
 const app = express();
 
+//In order to use expressjs's delete functionality from form
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'));
+
 //We're using MongoDB, so we're using Mongoose for mapping JSON to MongoDB
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  movieTitle: String,
+  description: String
 });
+
+//Body parser to parse text
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 
 //We're also using Handlebars for ExpressJS
@@ -24,22 +37,67 @@ app.set('view engine', 'handlebars');
 //
 
 //Our mock array of projects
-// let reviews = [
-//     { title: "Great Review" },
-//     { title: "Next Review" }
-// ];
+let reviews2 = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+];
+
+
+//Delete a posted review
+app.delete('/reviews/:id', (req, res) => {
+  console.log("Delete review: " + req.params.id);
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  });
+});
+
+//When a new review is submitted and user is sent back to /reviews
+app.post('/reviews', (req, res) => {
+  console.log("Review before mongodb: " + req.body.title);
+
+  const review = new Review(req.body)
+
+  // review.save().then((review) => {
+  //   res.redirect('/');
+  // }).catch((err) => {
+  //   console.log(err.message)
+  // })
+
+  Review.create(req.body).then((review) => {
+    console.log("Review after mongodb" + review);
+    return review.save()
+  }).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  });
+});
 
 app.get('/reviews', (req, res) => {
-    res.render('reviews-index', { reviews });
+    res.render('reviews-index', { reviews2 });
+});
+
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+});
+
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', {review});
+  }).catch((err) => {
+    console.log(err.message);
+  });
 });
 
 //Index
 app.get('/', (req, res) => {
   Review.find().then((reviews) => {
     res.render('reviews-index', {reviews: reviews});
-    }).catch((err) => {
+  }).catch((err) => {
       console.log(err);
-    });
+  });
 });
 
 //Listens for port 3000: E.g. localhost:3000s
